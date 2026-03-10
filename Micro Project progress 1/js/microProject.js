f7a109016b47e31b725f1a82ddb25fdefa5e7971
midominguez10 <mdominguez10@sfsu.edu>
2026-03-10 09:42:06 -0700

    edit 2

--- Micro Project progress 1/js/microProject.js
+++ Micro Project progress 1/js/microProject.js
@@ -1,16 +1,43 @@
 let x = 500;
 let speed = 10;
-let d = 50;
+let d = 75;
 let moving = false;
 let timer = 100;
 
+let circleX, circleY;
+let squareX, squareY;
+let triangleX, triangleY;
+let shapeOrder = []
+
+let circlePenalty = 10;
+let squarePenalty = 20;
+let trianglePenalty = 30;
+
 function setup() {
     createCanvas(1000,1000)
+
+    circleX = width/2-300;
+    circleY = height/3;
+
+    squareX = width/2;
+    squareY = height/3;
+
+    triangleX = width/2+300;
+    triangleY = height/3;
+
+    shapeOrder = shuffle (["circle","square","triangle"]);
 }
 
 function draw (){
     background (220);
 
+    fill(255,0,0);
+    if (moving) {
+    drawShape(shapeOrder[0], circleX, circleY);
+    drawShape(shapeOrder[1], squareX, squareY);
+    drawShape(shapeOrder[2], triangleX, triangleY);
+    }
+    
     if (moving && timer > 0) {
         timer -= 1/60;
     }
@@ -40,7 +67,49 @@ function draw (){
 } 
 
 function keyPressed () {
-    if (key === ' ')
+    if (key === ' ') {
         moving = true;
+        let index;
+
+    if (abs(x - circleX) < 40){
+        index = 0;
+    }
+    if (abs(x-squareX) < 40){
+        index = 1;
+    }
+    if (abs(x - triangleX) < 40){
+        index = 2;
+    }
+
+    let shape = shapeOrder [index];
+
+    if (shape === "circle"){
+        timer -= circlePenalty
+    }
+    if (shape === "square"){
+        timer -= squarePenalty
+    }
+    if (shape === "triangle"){
+        timer -= trianglePenalty;
+    }
+    }
+}
+
+function drawShape(type, shapeX, shapeY) {
+    if (type === "circle"){
+        ellipse(shapeX,shapeY,80,80);
+    }
+    if (type === "square"){
+        rect (shapeX-40, shapeY-40,80,80);
+        
+    }
+    if (type==="triangle"){
+        triangle(
+            shapeX, shapeY-40,
+            shapeX-40, shapeY+40,
+            shapeX+40, shapeY+40
+        );
+       
+    }
 }
 
